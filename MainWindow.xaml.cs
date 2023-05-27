3e0b69488a53d91f3d95a5b2bc73dc4418f0ad9c
Allan Quintana <allan.quintana@gmail.com>
2023-05-27 08:40:35 -0400

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_EstudioSolemne2_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtNombre.Clear();
+            this.txtTelefono.Clear();   
+        }
+
+        private void btnGrabar_Click(object sender, RoutedEventArgs e)
+        {
+            string strNombre = this.txtNombre.Text.Trim();
+            string strTelefono = this.txtTelefono.Text.Trim();
+
+            string contacto = strNombre.ToUpper() + "," + strTelefono;
+            StreamWriter writer = new StreamWriter("MiAgenda.txt", true);
+            writer.WriteLine(contacto);
+            writer.Close();
+
+            MessageBox.Show("Contacto agregado");
+        }
+    }
+}
